48da0cb348ac8861b48d8ef9e33b7aa4d8c699cc
Kabir Singh Shekhawat <kabirsinghshekhawat1789@gmail.com>
2021-11-13 20:29:58 +0530

    fix: CORS whitelist

--- app.js
+++ app.js
@@ -19,80 +19,78 @@ const unhandledExceptionListener = require('./utils/unhandledExceptionListener')
 require('dotenv').config({path: path.join(__dirname, '/.env')});
 
 process.on('uncaughtException', err => {
-  unhandledExceptionListener('UNHANDLED EXCEPTION', err)
+    unhandledExceptionListener('UNHANDLED EXCEPTION', err)
 })
 
-/*-----------Global Middlewares------------*/
+const whitelist = ['https://oijpcr.org', 'https://oijpcr-front-end.pages.dev/']
 
-// Helmet
-
-const corsOrigin = 'http://localhost:3000'
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (whitelist.indexOf(origin) !== -1 || !origin) {
+            return callback(null, true)
+        }
+        callback(new AppError("Blocked by CORS", 403))
+    }
+}
 
 app.use(
-  helmet({
-    contentSecurityPolicy: false,
-  }),
+    helmet({
+        contentSecurityPolicy: false,
+    }),
 )
 
 // CORS
 app.use(cors({
-  credentials: true,
-  origin: corsOrigin
+    credentials: true,
+    origin: corsOptions
 }))
 
-// Rate Limiter
 const limiter = rateLimit({
-  windowMs: 60 * 60 * 1000,
-  max: 1000,
-  message: 'Too many requests, try again later',
+    windowMs: 60 * 60 * 1000,
+    max: 1000,
+    message: 'Too many requests, try again later',
 })
 
 app.use('*', limiter)
-
-// Method Override
 app.use(method_override('_method'))
-
-// cookie parser
 app.use(cookieParser())
-
-
-app.use(express.urlencoded({ limit: '5mb', extended: true }))
-app.use(express.json({ limit: '5mb' }))
+app.use(express.urlencoded({limit: '5mb', extended: true}))
+app.use(express.json({limit: '5mb'}))
 
 let mongoConnectionString
 
 if (process.env.NODE_ENV === 'prod') {
-  app.use(morgan('tiny'))
-  mongoConnectionString = process.env.MONGO_URI
+    app.use(morgan('tiny'))
+    mongoConnectionString = process.env.MONGO_URI
 } else {
-  app.use(morgan('dev'))
-  mongoConnectionString = 'mongodb://localhost/oijpcr'
+    app.use(morgan('dev'))
+    mongoConnectionString = 'mongodb://localhost/oijpcr'
 }
 
 
 const mongoOptions = {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
 }
 
 mongoose.connect(mongoConnectionString, mongoOptions)
-  .then(() => {
-    console.log('Connected to MongoDB: OIJPCR 🐦 🐦 🐦')
-  })
-  .catch(err => {
-    throw new Error(`Error Message: ${err.message}`)
-  })
+    .then(() => {
+        console.log('Connected to MongoDB: OIJPCR 🐦 🐦 🐦')
+    })
+    .catch(err => {
+        throw new Error(`Error Message: ${err.message}`)
+    })
 
 
 app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-  res.header('Access-Control-Allow-Credentials', true);
-  res.header('Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, X-PINGOTHER,Content-Type, Accept, Authorization'
-  );
-  next();
+    res.setHeader('Access-Control-Allow-Origin', whitelist);
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, X-PINGOTHER,Content-Type, Accept, Authorization'
+    );
+    next();
 });
 
 
@@ -103,27 +101,27 @@ app.use('/editor', editorRoute)
 
 // Serve static assets (react) in production
 if (process.env.NODE_ENV === 'prod') {
-  app.use(express.static('client/build'))
-  app.get('*', (req, res) => {
-    res.sendFile(
-      path.resolve(
-        __dirname,
-        'client',
-        'build',
-        'index.html'
-      )
-    )
-  })
+    app.use(express.static('client/build'))
+    app.get('*', (req, res) => {
+        res.sendFile(
+            path.resolve(
+                __dirname,
+                'client',
+                'build',
+                'index.html'
+            )
+        )
+    })
 }
 
 // 404 page
 app.get('*', (req, res) => {
-  res.status(404).send('<h1>404 Page Not Found</h1>')
+    res.status(404).send('<h1>404 Page Not Found</h1>')
 })
 
 // catch all
 app.all('*', (req, res, next) => {
-  next(new AppError(`${req.originalUrl} not found`, 404))
+    next(new AppError(`${req.originalUrl} not found`, 404))
 })
 
 // Error handler middleware
