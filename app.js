885cab23fdfa709c11f2698914ec7048cc5fcbd8
Yuvraj Singh Pathania <58564635+batman004@users.noreply.github.com>
2021-12-10 12:26:56 -0800

    Update app.js

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ process.on('uncaughtException', err => {
     unhandledExceptionListener('UNHANDLED EXCEPTION', err)
 })
 
-const whitelist = 'http://localhost:3000'
+const whitelist = 'https://oijpcr.org'
 
 const corsOptions = {
     origin: function (origin, callback) {
